b0f91004ae8350e34533726d772fdeb55e28ddda
Henri <oneiromancy.ai@gmail.com>
2021-01-30 22:27:18 +0000

    Updates

---
+++ precache-manifest.9a7344e6f69e45a9f269c5a2b138295e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a4c81c4e6a8df73b845",
+    "url": "/financial-times/static/css/main.4277a6a3.chunk.css"
+  },
+  {
+    "revision": "7a4c81c4e6a8df73b845",
+    "url": "/financial-times/static/js/main.7a4c81c4.chunk.js"
+  },
+  {
+    "revision": "f3813bc3492d7de6d8c7",
+    "url": "/financial-times/static/js/1.f3813bc3.chunk.js"
+  },
+  {
+    "revision": "1f02f45eacb045d0ccf1",
+    "url": "/financial-times/static/js/runtime~main.1f02f45e.js"
+  },
+  {
+    "revision": "013a2076e268cb613853e9d9f576e328",
+    "url": "/financial-times/index.html"
+  }
+];
